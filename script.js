65095e9dd8c2707f1d700d260f89007de4bf3863
James Corcoran <jpcsoccer1234@gmail.com>
2021-03-08 21:07:33 -0500

    created function for timer and function that removes the start button after being clicked

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
+// startButton.addEventListener("click", function(){
+//     document.getElementById("timer").style.visibility="visible";
+// })
+// when i click start:
+//      the timer appears and starts to countdown
+//      the <p> section goes away and the first question is displayed
+//      
+// i need to be able to select an answer 
+// then to click submit:
+//    after submittiing:
+//      if correct add to score 
+//      if incorrect subtracr from time
+//
+// when timer runs out:
+//      a message with the score and asing for initials is displayed
+//      input box is displayed 
+//      high scores are displayed 
+
 let startButton = document.querySelector("button");
-// let timerElement = document.getElementById("timer");
+let timerElement = document.getElementById("timer");
+
+function countdown() {
+    let timeLeft = 30;
+    let timeInterval = setInterval(function(){
+        if(timeLeft > 1){
+            timerElement.textContent = timeLeft;
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerElement.textContent = timeLeft;
+            timeLeft--;
+        } else {
+            timerElement.textContent = "time is up";
+            clearInterval(timeInterval);
+            // displayQuestion();  
+        }
+    },1000);
+}
 
 startButton.addEventListener("click", function(){
-    document.getElementById("timer").style.visibility="visible";
-})
+    countdown();
+    startButton.style.visibility = "hidden";
+});
