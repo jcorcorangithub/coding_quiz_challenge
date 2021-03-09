f2e012349abe1fda84e046c9dd454fbc29fb2e2c
James Corcoran <jpcsoccer1234@gmail.com>
2021-03-09 01:57:00 -0500

    worked on javascript function for questions and answers

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// startButton.addEventListener("click", function(){
-//     document.getElementById("timer").style.visibility="visible";
-// })
-// when i click start:
-//      the timer appears and starts to countdown
-//      the <p> section goes away and the first question is displayed
-//      
 // i need to be able to select an answer 
 // then to click submit:
 //    after submittiing:
@@ -15,9 +8,55 @@
 //      a message with the score and asing for initials is displayed
 //      input box is displayed 
 //      high scores are displayed 
-
 let startButton = document.querySelector("button");
 let timerElement = document.getElementById("timer");
+let questions = document.querySelector("p");
+
+let arrayOfQuestions = ["question1","q2","q3","q4","q5","q6","q7","q8"];
+
+let uList = document.querySelector("ul");
+let li1 = document.createElement("li");
+let li2 = document.createElement("li");
+let li3 = document.createElement("li");
+let li4 = document.createElement("li");
+
+
+
+function questionFunction(){
+    for(i = 0; i < arrayOfQuestions.length; i++){
+        questions.textContent = arrayOfQuestions[i];
+        console.log(i);
+
+        uList.appendChild(li1);
+        uList.appendChild(li2);
+        uList.appendChild(li3);
+        uList.appendChild(li4);
+        
+        // these would also iterate 
+        li1.textContent = arrayOfQuestions[i]; 
+        li2.textContent = arrayOfQuestions[i];
+        li3.textContent = arrayOfQuestions[i];
+        li4.textContent = arrayOfQuestions[i];
+
+    }
+    // this will display question after question 
+
+    //for each question:
+    //      create 4 answers, each as an element on the html file (maybe a list)
+    //      each answer is highlughted when clicked
+    //      a submit button must also be created 
+    //
+    //      <p> input the question into this tag
+    //      create and then append an <ol> with 4 <li> for each question (hover and click effect)
+    //      the submit button could be the startbutton 
+    //
+    //
+    //
+    //
+    //
+
+    // return "question1";
+}
 
 function countdown() {
     let timeLeft = 30;
@@ -30,13 +69,16 @@ function countdown() {
             timeLeft--;
         } else {
             timerElement.textContent = "time is up";
-            clearInterval(timeInterval);
-            // displayQuestion();  
+            clearInterval(timeInterval);  
         }
     },1000);
 }
 
 startButton.addEventListener("click", function(){
-    countdown();
-    startButton.style.visibility = "hidden";
+    timerElement.style.visibility="visible";
+    startButton.style.display= "none";
+    countdown(); 
+    questionFunction();
+    
+    
 });
