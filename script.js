0f9df5db16c53020df2c321dbb96df5b7f4ff0f0
James Corcoran <jpcsoccer1234@gmail.com>
2021-03-08 19:23:39 -0500

    added some content with styling. created a button that uses a js function

---
+++ script.js
@@ -0,0 +1,6 @@
+let startButton = document.querySelector("button");
+// let timerElement = document.getElementById("timer");
+
+startButton.addEventListener("click", function(){
+    document.getElementById("timer").style.visibility="visible";
+})
