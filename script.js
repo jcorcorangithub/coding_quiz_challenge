ffd2c7a9e9875df330cfbdfdfaaffb1063e12bf2
James Corcoran <jpcsoccer1234@gmail.com>
2021-03-11 14:34:37 -0500

    finished functionality that changes the questions and answers after an answer is submitted

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ let arrayOfSecondChoices =  ["2c1", "2c2",  "2c3",  "2c4",  "2c5"];
 let arrayOfThirdChoices =   ["3c1", "3c2",  "3c3",  "3c4",  "3c5"];
 let arrayOfFourthChoices =  ["4c1", "4c2",  "4c3",  "4c4",  "4c5"];
 let arrayOfCorrectAnswers = ["2c1", "2c2",  "4c3",  "1c4",  "3c5"];
-//let arrayIndexTracker = [0,1,2,3,4];          not needed
 
 let uList = document.querySelector("ul");
 let li1 = document.createElement("li");
@@ -25,24 +24,8 @@ let submitButton = document.createElement("button");
 submitButton.textContent = "submit answer";
 let li6 = document.createElement("li");
 let scoreTracker = 0;
-let timeLeft = 0;
 
 
-function countdown() {
-    timeLeft = 30;
-    let timeInterval = setInterval(function(){
-        if(timeLeft > 1){
-            timerElement.textContent = timeLeft;
-            timeLeft--;
-        } else if (timeLeft === 1) {
-            timerElement.textContent = timeLeft;
-            timeLeft--;
-        } else {
-            timerElement.textContent = "time is up";
-            clearInterval(timeInterval);  
-        }
-    },1000);
-}
 
 startButton.addEventListener("click", function(){
     timerElement.style.visibility="visible";
@@ -57,37 +40,67 @@ startButton.addEventListener("click", function(){
     submitButton.disabled = true; // this keeps the submit button disabled until an answer is chosen
 
     // need a way to iterate to next question after submit button is clicked 
-        questions.textContent = arrayOfQuestions[i];
-                
-        li1.textContent = arrayOfFirstChoices[i];
-                li1.addEventListener("click",function(){change(li1)});
-        li2.textContent = arrayOfSecondChoices[i];
-                li2.addEventListener("click",function(){change(li2)});        
-        li3.textContent = arrayOfThirdChoices[i];
-                li3.addEventListener("click",function(){change(li3)}); 
-        li4.textContent = arrayOfFourthChoices[i];
-                li4.addEventListener("click",function(){change(li4)}); 
     
+        let i = 0;
+    
+    qAndaChanger(i);
+
+    submitButton.onclick = function() {
+        if(chosenAnswer.textContent!==arrayOfCorrectAnswers[i]){
+            uList.appendChild(li6).textContent = "wrong answer: minus 3 seconds from timer";
+            li6.style.color = "red";
+            timeLeft = timeLeft-3;
+            submitButton.disabled = true;
+            i++;
+            chosenAnswer.style.backgroundColor = "white";
+            qAndaChanger(i);
+            //chosenAnswer = null;
+        } else {
+            uList.appendChild(li6).textContent = "Correct!";
+            li6.style.color = "green";
+            scoreTracker++;
+            submitButton.disabled = true;
+            //chosenAnswer = null;
+            i++;
+            chosenAnswer.style.backgroundColor = "white";
+            qAndaChanger(i);
+        }
+    }
+
+        // questions.textContent = "Game Over"
         
-        
-                submitButton.onclick = function() {
-                    if(chosenAnswer.textContent!==arrayOfCorrectAnswers[i]){
-                        uList.appendChild(li6).textContent = "wrong answer: minus 3 seconds from timer";
-                        li6.style.color = "red";
-                        timeLeft = timeLeft-3;
-                        submitButton.disabled = true;
-                        //chosenAnswer = null;
-                    } else {
-                        uList.appendChild(li6).textContent = "Correct!";
-                        li6.style.color = "green";
-                        scoreTracker++;
-                        submitButton.disabled = true;
-                        //chosenAnswer = null;
-                    }
-                } 
-    
-    
+
 });
+
+    let timeLeft = 0;
+    function countdown() {
+        timeLeft = 5;
+        let timeInterval = setInterval(function(){
+            if(timeLeft > 1){
+                timerElement.textContent = timeLeft;
+                timeLeft--;
+            } else if (timeLeft === 1) {
+                timerElement.textContent = timeLeft;
+                timeLeft--;
+            } else {
+                timerElement.textContent = "time is up";
+                clearInterval(timeInterval);  
+            }
+        },1000);
+    }
+
+    function qAndaChanger(i){
+        questions.textContent = arrayOfQuestions[i];
+
+        li1.textContent = arrayOfFirstChoices[i];
+            li1.addEventListener("click",function(){change(li1)});
+        li2.textContent = arrayOfSecondChoices[i];
+            li2.addEventListener("click",function(){change(li2)});        
+        li3.textContent = arrayOfThirdChoices[i];
+            li3.addEventListener("click",function(){change(li3)}); 
+        li4.textContent = arrayOfFourthChoices[i];
+            li4.addEventListener("click",function(){change(li4)});
+    } 
 
     let chosenAnswer;
 
@@ -98,22 +111,4 @@ startButton.addEventListener("click", function(){
         a.style.backgroundColor= "gray";
         submitButton.disabled = false;
         chosenAnswer = a;
-        console.log(chosenAnswer);
     }
-
-    // function qAndADisplay(i){
-
-    // questions.textContent = arrayOfQuestions[i];
-                
-    //     li1.textContent = arrayOfFirstChoices[i];
-    //             li1.addEventListener("click",function(){change(li1)});
-    //     li2.textContent = arrayOfSecondChoices[i];
-    //             li2.addEventListener("click",function(){change(li2)});        
-    //     li3.textContent = arrayOfThirdChoices[i];
-    //             li3.addEventListener("click",function(){change(li3)}); 
-    //     li4.textContent = arrayOfFourthChoices[i];
-    //             li4.addEventListener("click",function(){change(li4)}); 
-        
-        
-                
-    
